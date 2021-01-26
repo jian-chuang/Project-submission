980c032f1a43ae3c49e6d32f44580b085f4f86cc
创 <chuang@qq.com>
2021-01-26 22:37:45 +0800

    项目改良2

--- controller/listController.js
+++ controller/listController.js
@@ -1,5 +1,32 @@
 let listController = {};
+// let mockdata = require('../mockData/mock-list.json')
+const dbquery = require("../model/sqlmodel");
+
 listController.list = (req,res)=>{
-    res.render("listadd.html")
+    res.render("list.html")
+}
+listController.getlist = async (req,res)=>{
+    let {page,limit:pagesize} = req.query;
+    let offest = (page - 1) * pagesize;
+    // 分页
+    let sql = `select * from article limit ${offest},${pagesize}`;
+    // 查询总数据的条数
+    let sql2 = `select count(*) as count from article`;
+    // 分页sql语句调用
+    let promise = dbquery(sql);
+    // 查询总数据条数sql语句调用；返回结果是数组[],数组中的值是一个对象
+    let promise2 = dbquery(sql2);
+    // 封装promise函数，串型变并型
+    let result = await Promise.all([promise,promise2]);
+    let data = result[0];
+    let count = result[1][0].count
+    let response = {
+        code:0,
+        count:count,
+        data:data,
+        msg:''
+    }
+    res.json(response)
+    // res.json(mockdata)
 }
 module.exports = listController
--- router/router.js
+++ router/router.js
@@ -3,31 +3,40 @@ var router = express.Router();
 const SortController = require("../controller/SortController.js")
 const indexController = require('../controller/indexController.js')
 const listController = require('../controller/listController')
+
+
 // 主页接口
 router.get('/index',indexController.index)
 // 定向重制主页接口
 router.get('/',indexController.root)
+
+
 // 文章列表接口
 router.get("/list",listController.list)
-// 文章分类接口
+
+router.get('/getlist',listController.getlist)
+// 文章分类接口(跳转页面)
 router.get('/sort',SortController.sort)
-// 文章分类数据接口
-router.get("/getsort",SortController.getsort)
-// 文章分类删除数据接口
-router.post('/deleSort',SortController.deleSort)
-// 文章分类自定义添加分类接口
+
+// 文章分类自定义添加分类接口(跳转添加数据页面)
 router.get('/addSort',SortController.addSort)
-// 文章分类增加数据接口
+
+// 文章分类编辑页面接口(跳转编辑操作页面)
+router.get('/edit',SortController.edit)
+
+// 文章分类数据接口(数据渲染页面)
+router.get("/getsort",SortController.getsort)
+
+// 文章分类删除数据接口(数据删除操作)
+router.post('/deleSort',SortController.deleSort)
+
+// 文章分类增加数据接口(获取添加数据操作)
 router.post('/increase',SortController.increase)
 
-
-
-// 文章分类编辑页面接口
-router.get('/edit',SortController.edit)
-//文章分类编辑获取渲染数据接口
+//文章分类编辑获取渲染数据接口(数据回显操作)
 router.get('/getEdit',SortController.getEdit)
 
-// 文章分类编辑更新数据接口
+// 文章分类编辑更新数据接口(数据更新添加操作)
 router.post('/upDate',SortController.upDate)
 
 
